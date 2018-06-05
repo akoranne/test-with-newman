702d0547c8f6c8f7b7b8bdb8499849a252e25abd
Ajay Koranne <akoranne@gmail.com>
2018-06-04 19:15:09 -0500

    added ci pipeline for running newman tests

---
+++ ci/tasks/test-echo/task.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e # fail fast
+#	set -x # print commands
+
+echo ""
+echo " .. upgrading 'npm' "
+echo ""
+npm install -g npm
+
+echo ""
+echo " .. install 'newman' "
+echo ""
+npm install -g newman
+
+echo ""
+echo " .. Running newman tests"
+echo ""
+
+cd service-repo
+
+# execute newman tests
+newman run PostmanEcho.postman_collection
+
+echo ""
+echo " Build completed!!!"
+echo ""
