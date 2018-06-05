839339f213def285d8858f84ee30993e314b70b7
Ajay Koranne <akoranne@gmail.com>
2018-06-04 20:38:30 -0500

    trial-error

---
+++ ci/tasks/test-echo/task-1.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e # fail fast
+#   set -x # print commands
+
+whoami;
+
+which npm;
+
+echo ""
+echo " .. Running newman tests"
+echo ""
+
+ls -lR
+
+echo ""
+echo " Build completed!!!"
+echo ""
--- ci/tasks/test-echo/task.sh
+++ ci/tasks/test-echo/task.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 set -e # fail fast
-set -x # print commands
+#   set -x # print commands
 
-echo ""
-echo " .. upgrading 'npm' "
-echo ""
-npm install -g npm
+whoami;
 
-echo ""
-echo " .. install 'newman' "
-echo ""
-npm install -g newman
+which npm;
+
+
+
+# echo ""
+#echo " .. upgrading 'npm' "
+#echo ""
+#npm install -g npm
+
+#echo ""
+#echo " .. install 'newman' "
+#echo ""
+#npm install -g newman
 
 echo ""
 echo " .. Running newman tests"
@@ -20,7 +26,7 @@ echo ""
 cd service-repo
 
 # execute newman tests
-newman run PostmanEcho.postman_collection
+#       newman run PostmanEcho.postman_collection
 
 echo ""
 echo " Build completed!!!"
